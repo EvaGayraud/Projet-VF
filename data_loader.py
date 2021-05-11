66b7b09122c8ea48113d9a32e05421c222101ee0
ega95 <evagayraud@hotmail.fr>
2021-05-11 09:14:44 +0200

    increase accuracy

--- data_loader.py
+++ data_loader.py
@@ -1,21 +1,41 @@
 import pandas as pd
+from tqdm.notebook import tqdm
+
+url_dict = {
+    '2020': 'https://www.data.gouv.fr/fr/datasets/r/90a98de0-f562-4328-aa16-fe0dd1dca60f',
+    '2019': 'https://www.data.gouv.fr/fr/datasets/r/3004168d-bec4-44d9-a781-ef16f41856a2',
+    '2018': 'https://www.data.gouv.fr/fr/datasets/r/1be77ca5-dc1b-4e50-af2b-0240147e0346',
+    '2017': 'https://www.data.gouv.fr/fr/datasets/r/7161c9f2-3d91-4caf-afa2-cfe535807f04',
+    '2016': 'https://www.data.gouv.fr/fr/datasets/r/0ab442c5-57d1-4139-92c2-19672336401c',
+}
 
 
-def load_house_attributes(url):
-    # get the dataframe
-    df0 = pd.read_csv(url, low_memory=False, sep="|")
-    df = df0.copy()
-    df = df[df['Code departement'] == '75']
+def get_subsample():
+    data_all_year = []
+    for year, url in tqdm(url_dict.items()):
+        data_ = pd.read_csv(url, low_memory=False, sep="|")
 
-    # drop columns based on number of null
-    df = df.drop(['Code service CH', 'Reference document', '1 Articles CGI', '2 Articles CGI', '3 Articles CGI',
-                  '4 Articles CGI', '5 Articles CGI', 'B/T/Q', 'Prefixe de section',  'No Volume', '1er lot',
-                  'Surface Carrez du 1er lot', '2eme lot', 'Surface Carrez du 2eme lot', '3eme lot',
-                  'Surface Carrez du 3eme lot', '4eme lot', 'Surface Carrez du 4eme lot', '5eme lot',
-                  'Surface Carrez du 5eme lot',  'Identifiant local', 'Code voie', 'Code type local',
-                  'No Volume',  'Nature culture', 'Nature culture speciale', 'Surface terrain',
-                  'No voie', 'Voie', 'No plan'], axis=1)
+        # drop columns based on number of null
+        data_ = data_.drop(['Code service CH', 'Reference document', '1 Articles CGI', '2 Articles CGI', '3 Articles CGI',
+                      '4 Articles CGI', '5 Articles CGI', 'B/T/Q', 'Prefixe de section', 'No Volume', '1er lot',
+                      'Surface Carrez du 1er lot', '2eme lot', 'Surface Carrez du 2eme lot', '3eme lot',
+                      'Surface Carrez du 3eme lot', '4eme lot', 'Surface Carrez du 4eme lot', '5eme lot',
+                      'Surface Carrez du 5eme lot', 'Identifiant local', 'Code voie', 'Code type local',
+                      'No Volume', 'Nature culture', 'Nature culture speciale', 'Surface terrain',
+                      'No voie', 'Voie', 'No plan'], axis=1)
 
+        data = data_[data_['Code departement'] == '75']
+        del data_
+        data['year'] = year
+        data_all_year.append(data)
+        del data
+
+    df = pd.concat(data_all_year)
+
+    return df
+
+
+def load_house_attributes(df):
     df.drop_duplicates()
     df.dropna(inplace=True)
 
--- graph_paris.py
+++ graph_paris.py
@@ -1,9 +1,66 @@
 import plotly.express as px
 import data_loader
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
 
-df_paris = data_loader.load_house_attributes('https://www.data.gouv.fr/fr/datasets/r/90a98de0-f562-4328-aa16-fe0dd1dca60f')
+app = dash.Dash(__name__)
 
-fig = px.box(df_paris, x='Commune', y='Valeur fonciere', hover_name='Type local')
-fig.show()
+df = data_loader.load_house_attributes('https://www.data.gouv.fr/fr/datasets/r/90a98de0-f562-4328-aa16-fe0dd1dca60f')
+
+app.layout = html.Div([
+
+    html.H1("DVF Paris", style={'text-align': 'center'}),
+
+    dcc.Dropdown(id="months",
+                 options=[
+                     {"label": "Jan", "value": 1},
+                     {"label": "Fev", "value": 2},
+                     {"label": "Mars", "value": 3},
+                     {"label": "Avril", "value": 4},
+                     {"label": "Mai", "value": 5},
+                     {"label": "Juin", "value": 6},
+                     {"label": "Juillet", "value": 7},
+                     {"label": "Août", "value": 8},
+                     {"label": "Sep", "value": 9},
+                     {"label": "Oct", "value": 10},
+                     {"label": "Nov", "value": 11},
+                     {"label": "Dec", "value": 12}],
+                 multi=False,
+                 value="Jan",
+                 style={'width': "40%"}
+                 ),
+
+    html.Div(id='output_container', children=[]),
+    html.Br(),
+
+    dcc.Graph(id='dispersion', figure={})
+
+])
+
+@app.callback(
+    [Output(component_id='output_container', component_property='children'),
+     Output(component_id='dispersion', component_property='figure')],
+    [Input(component_id='months', component_property='value')]
+)
+def update_graph(option_slctd):
+    print(option_slctd)
+    print(type(option_slctd))
+
+    container = "The month chosen by user was: {}".format(option_slctd)
+
+    dff = df.copy()
+    dff = dff[dff["months"] == option_slctd]
+
+    fig = px.box(dff, x='Commune', y='Valeur fonciere', hover_name='Type local')
+
+    return container, fig
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
 
 
